cee71b75a68d38dc1ca28251beab0437a2029841
Igor Eulalio <igoreulalio.ie@gmail.com>
2022-03-09 15:51:06 -0300

    initial commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "default"
+  region  = "sa-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.21"
+    }
+  }
+}
